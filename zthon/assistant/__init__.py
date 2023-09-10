3cc64fd75c47513a0c15056edd123954aeccf591
ابراهيم <143281628+vbajw@users.noreply.github.com>
2023-09-10 21:21:45 +0300

    Add files via upload

---
+++ zthon/assistant/__init__.py
@@ -0,0 +1,5 @@
+from zthon import BOTLOG, BOTLOG_CHATID, zedub
+
+from ..Config import Config
+from ..core.inlinebot import *
+                                                                                                                                                                                                                                                                 
